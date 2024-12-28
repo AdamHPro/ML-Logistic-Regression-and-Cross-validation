d399c8fbb83059171b1ac487f25c371de16081fb
AdamHPro <adam.hachicha@student-cs.fr>
2024-12-28 20:26:51 +0100

    Update computeCost.py

--- computeCost.py
+++ computeCost.py
@@ -5,12 +5,7 @@ def computeCost(theta, X, y):
     # Computes the cost using theta as the parameter 
     # for logistic regression. 
     m = X.shape[0] # number of training examples
-    
-    
-    # ====================== YOUR CODE HERE ======================
-    # Instructions: Calculate the error J of the decision boundary
-    #               that is described by theta (see the assignment 
-    #               for more details).
+
     J = 0
     
     Xtheta = np.matmul(X, theta)
@@ -18,13 +13,4 @@ def computeCost(theta, X, y):
         J += y[i]*np.log(sigmoid(Xtheta[i])) + (1-y[i])*np.log(1-sigmoid(Xtheta[i]))
     J = -J/m
         
-        
-    
-    
-    
-
-    
-    
-    
-    # =============================================================
     return J
